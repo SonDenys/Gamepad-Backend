25de34b2b82b47876a5061c94c76ed49c766f1bd
Denis Son <sondenis90@gmail.com>
2021-08-18 16:30:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+const isAuthenticated = require("./middlewares/isAuthenticated");
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+// Mongoose connect
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+// Import de Cloudinary
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+//   secure: true,
+// });
+
+// Import des routes
+const userRoutes = require("./routes/user");
+app.use(userRoutes);
+
+const resultsRoutes = require("./routes/results");
+app.use(resultsRoutes);
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found !" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Started");
+});
