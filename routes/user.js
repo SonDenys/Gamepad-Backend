cf142759af45c63a1a950aef991587471eabf86e
Denis Son <sondenis90@gmail.com>
2021-08-19 10:26:03 +0200

    update script

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const isAuthenticated = require("../middlewares/isAuthenticated");
+
 // Import models
 const User = require("../models/User");
 
